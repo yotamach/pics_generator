da9743d7a1475e8f6e4171cc26ff6da7c716ba78
Yotam Achrak <yotam2585@gmail.com>
2019-06-17 14:54:21 +0300

    make it better

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/your_app_name'));
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/PicsGenerator/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
